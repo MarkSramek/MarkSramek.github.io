62f06cbb565511b7b431baa1c5e7301237bb707c
Neil Inglese <neilinglese@gmail.com>
2019-07-16 21:54:23 -0500

    another one

--- scripts/blueimp-gallery-youtube.js
+++ scripts/blueimp-gallery-youtube.js
@@ -197,7 +197,7 @@
       var videoId = this.getItemProperty(obj, options.youTubeVideoIdProperty)
       if (videoId) {
         if (this.getItemProperty(obj, options.urlProperty) === undefined) {
-          obj[options.urlProperty] = 'https://www.youtube.com/watch?v=' + videoId
+          obj[options.urlProperty] = 'https://www.youtube.com/embed/' + videoId
         }
         if (
           this.getItemProperty(obj, options.videoPosterProperty) === undefined
