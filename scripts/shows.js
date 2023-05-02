0751f5aeb314528d2fcb8356ae4fb807cea0122e
MarkSramek <52712776+MarkSramek@users.noreply.github.com>
2023-05-02 08:56:30 -0500

    Update shows.js

--- scripts/shows.js
+++ scripts/shows.js
@@ -14,10 +14,10 @@ var shows = [
         Venue: "Joe's Saloon, 9220 47th St, Brookfield",
     },
     {
-        Date: "For current shows please access Mark Sramek on Facebook"
-        Time: " "
-        Who: " "
-        Venue: " "
+        Date: "For current shows"
+        Time: "please access"
+        Who: "Mark Sramek on Facebook"
+        Venue: "Thanks!"
     },
        
   ];
