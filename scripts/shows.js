cce7d93fa0cb627c384a212f136d6ec2989015b2
MarkSramek <52712776+MarkSramek@users.noreply.github.com>
2023-05-02 07:55:48 -0500

    Update shows.js

--- scripts/shows.js
+++ scripts/shows.js
@@ -6,38 +6,14 @@
 //     Venue: "Where ex: Columbia College Chicago",
 // },
 var shows = [
-    
-    {
-        Date: "Jan. 11, 2022",
-        Time: "7pm-11pm",
-        Who:  "Taylor King Jam Night - Always lots of fun to sit in with this band! Come on out and listen to some great music!",
-        Venue: "Oceanview Inn & Sports Pub, MM 84.5 Islamorada, Fla.",
-    },
-    {
-        Date: "Aug. 12, Oct. 2, Dec. 9",
-        Time: "6:30pm-9:30pm",
-        Who:  "Mark & Ken",
-        Venue: "The Elm restaurant, 23 W. Harris, LaGrange, IL",
-    },
-    {
-        Date: "May 29, 30 & June 5, 6",
-        Time: "11am-2pm",
-        Who:  "Salt Creek Boys",
-        Venue: "Mary's Morning Mixup, 9110 Broadway, Brookfield, IL",
-    },
+  
     {
         Date: "3rd Friday of every month starting June 18th",
         Time: "8:00pm-11:00pm",
-        Who:  "Salties Host Open Mic",
+        Who:  "Mark Sramek & Co. Host Open Mic",
         Venue: "Joe's Saloon, 9220 47th St, Brookfield",
     },
-    {
-        Date: "July 24",
-        Time: "7pm-10pm",
-        Who:  "Mark Sramek with Mardi & Arthur",
-        Venue: "Bad Dad Brewing Co, 407 W. Washington St., Fairmont, Indiana",
-    },
-    
+       
   ];
 
 
